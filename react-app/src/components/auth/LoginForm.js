4625d804e251c430d553413edaa0c0e87a58015a
Austin Williams <austinwill264@gmail.com>
2021-08-16 12:43:10 -0700

    added error handler

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import Errors from '.././Errors';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,37 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <>
+        <Errors />
+        <form onSubmit={onLogin}>
+        <div>
+            {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+            ))}
+        </div>
+        <div>
+            <label htmlFor='email'>Email</label>
+            <input
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+            />
+        </div>
+        <div>
+            <label htmlFor='password'>Password</label>
+            <input
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+            />
+            <button type='submit'>Login</button>
+        </div>
+        </form>
+    </>
   );
 };
 
