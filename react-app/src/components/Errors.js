26e2075b6164d3958d2e0046ce109a18d25df6d8
Austin Williams <austinwill264@gmail.com>
2021-08-16 20:56:06 -0700

    navbar styled

--- react-app/src/components/Errors.js
+++ react-app/src/components/Errors.js
@@ -7,7 +7,7 @@ const Errors = () => {
     return (
         <div>
             {errors?.map((error, ind) => {
-                <div className='error' key={ind}>
+                return <div className='error' key={ind}>
                     {error}
                 </div>
             })}
