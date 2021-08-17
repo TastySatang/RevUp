825d67346821f8d72994352fcd5495e568da9eaf
junyeoble <junyeoble@gmail.com>
2021-08-17 11:04:17 -0700

    confirmed read working

--- react-app/src/pages/events.js
+++ react-app/src/pages/events.js
@@ -1,5 +1,25 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getEvents } from "../store/events"
+
 export default function EventsPage() {
+    const dispatch = useDispatch();
+    const events = useSelector((state) => Object.values(state.events))
+
+    useEffect(() => {
+        dispatch(getEvents());
+    }, [dispatch])
+    console.log(events)
+
     return (
-        <h1>Events Page</h1>
+        <>
+            {events.map((event, idx) => {
+                return(
+                    <div key={idx}>
+                        {event.name}
+                    </div>
+                )
+            })}
+        </>
     )
 }
