9299b1518121e148240bbb41ccf5c8c390b83503
Austin Williams <austinwill264@gmail.com>
2021-08-20 16:29:44 -0700

    rsvps work for all months

--- react-app/src/components/Calendar.js
+++ react-app/src/components/Calendar.js
@@ -18,8 +18,18 @@ const CalendarComponent = () => {
     }, [dispatch])
 
     const strToNum = (str) => {
-
-        if (str === 'Aug') return '8'
+        if (str === 'Jan') return '1';
+        if (str === 'Feb') return '2';
+        if (str === 'Mar') return '3';
+        if (str === 'Apr') return '4';
+        if (str === 'May') return '5';
+        if (str === 'Jun') return '6';
+        if (str === 'Jul') return '7';
+        if (str === 'Aug') return '8';
+        if (str === 'Sep') return '9';
+        if (str === 'Oct') return '10';
+        if (str === 'Nov') return '11';
+        if (str === 'Dec') return '12';
     }
 
     return (
