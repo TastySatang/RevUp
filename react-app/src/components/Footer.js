91c80c6e84e762282a284ed4c654683048a6ad8d
Austin Williams <austinwill264@gmail.com>
2021-10-06 10:57:59 -0700

    added socials

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -21,15 +21,15 @@ export default function Footer() {
         </div>
       </div>
       <div>
-        <div className='social__name'>Placeholder</div>
+        <div className='social__name'>Austin Williams</div>
         <div className='socials'>
-          <a href='#0' target='_blank'>
+          <a href='https://github.com/AustinW24' target='_blank' rel="noreferrer">
             <i className="fab fa-github" aria-hidden="true" />
           </a>
-          <a href='#0' target='_blank'>
+          <a href='https://www.linkedin.com/in/austin-williams-44a6a21b8/' target='_blank' rel="noreferrer">
             <i className="fab fa-linkedin" aria-hidden="true" />
           </a>
-          <a href="#0" target='_blank'>
+          <a href="https://angel.co/u/austin-williams-31" target='_blank' rel="noreferrer">
             <i className="fab fa-angellist" aria-hidden="true" />
           </a>
         </div>
